3fd27d3b0c91add8cb65bfb3a49f533996999e54
matsur <ru.far2014@gmail.com>
2021-10-21 00:14:04 +0500

    Выполнил 2 задание

---
+++ .sql
@@ -0,0 +1,21 @@
+﻿SELECT products.name, categories.name FROM products
+LEFT JOIN category_product ON products.id = category_product.product_id
+LEFT JOIN categories ON categories.id = category_product.category_id;
+
+/* Предполагаемая схема базы данных */
+CREATE TABLE products(
+    id int,
+    name char(20)
+);
+CREATE TABLE categories(
+    id int,
+    name char(20)
+);
+CREATE TABLE category_product(
+    category_id int,
+    product_id int,
+    CONSTRAINT FK_category_id FOREIGN KEY(category_id)
+    REFERENCES categories(id),
+    CONSTRAINT FK_product_id FOREIGN KEY(product_id)
+    REFERENCES products(id)
+);
